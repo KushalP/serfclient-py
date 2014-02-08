3a4533deb193fe3aceb9181e6a69d3a5d3e7ec87
Kushal Pisavadia <kushi.p@gmail.com>
2014-02-08 16:00:48 +0000

    Generate a setup.py for the serfclient module
    So it can be install using distutils or pip.

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from serfclient import __version__
+
+try:
+    from setuptools import setup
+    from setuptools.command.test import test as TestCommand
+
+    class PyTest(TestCommand):
+        def finalize_options(self):
+            TestCommand.finalize_options(self)
+            self.test_args = []
+            self.test_suite = True
+
+        def run_tests(self):
+            import pytest
+            errno = pytest.main(self.test_args)
+            sys.exit(errno)
+except ImportError:
+    from distutils.core import setup
+    PyTest = lambda x: x
+
+try:
+    long_description = open(os.path.join(os.path.dirname(__file__),
+                                         'README.rst')).read()
+except:
+    long_description = None
+
+setup(
+    name='serfclient',
+    version=__version__,
+    description='Python client for Serf orchestration tool',
+    long_description=long_description,
+    url='TBD',
+    author='Kushal Pisavadia',
+    author_email='kushal@violentlymild.com',
+    maintainer='Kushal Pisavadia',
+    maintainer_email='kushal@violentlymild.com',
+    keywords=['Serf', 'orchestration', 'service discovery'],
+    license='MIT',
+    packages=['serfclient'],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest}
+)
