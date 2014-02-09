a983d04249b3a4369390baad1e8627ccf55d9ec0
Kushal Pisavadia <kushi.p@gmail.com>
2014-02-09 23:11:07 +0000

    SerfClient initialises a connection and make a handshake

--- serfclient/client.py
+++ serfclient/client.py
@@ -1,3 +1,7 @@
+from connection import SerfConnection
+
 class SerfClient(object):
     def __init__(self, host='localhost', port=7373):
         self.host, self.port = host, port
+        self.connection = SerfConnection(host=self.host, port=self.port)
+        self.connection.handshake()
--- tests/test_client.py
+++ tests/test_client.py
@@ -12,3 +12,7 @@ class TestSerfClientCommands(object):
         serf = client.SerfClient()
         assert serf.host == 'localhost'
         assert serf.port == 7373
+
+    def test_initialises_a_serf_connection_on_creation(self):
+        serf = client.SerfClient()
+        assert serf.connection is not None
