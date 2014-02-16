8958e48c80734284e84484714c98370696975ad9
Kushal Pisavadia <kushi.p@gmail.com>
2014-02-16 22:21:55 +0000

    Added some documentation for the methods we have

--- serfclient/client.py
+++ serfclient/client.py
@@ -13,16 +13,26 @@ class SerfClient(object):
         self.connection.handshake()
 
     def event(self, name, payload, coalesce=True):
+        """
+        Send an event to the cluster. Can take an optional payload as well,
+        which will be sent in the form that it's provided.
+        """
         return self.connection.call(
             'event',
             {'Name': name, 'Payload': payload, 'Coalesce': coalesce})
 
     def force_leave(self, name):
+        """
+        Force a node to leave the cluster.
+        """
         return self.connection.call(
             'force-leave',
             {"Node": name})
 
     def join(self, location):
+        """
+        Join another cluster by provided a list of ip:port locations.
+        """
         if not isinstance(location, (list, tuple)):
             location = [location]
         return self.connection.call(
