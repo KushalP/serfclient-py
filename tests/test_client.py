460ba90fb9b69fe51904bad99d500cacc04d1b51
Kushal Pisavadia <kushi.p@gmail.com>
2014-02-23 13:16:11 +0000

    Replace common test instantiations with pytest fixtures
    Removing some duplication in the test source code.

--- tests/test_client.py
+++ tests/test_client.py
@@ -7,45 +7,39 @@ class TestSerfClientCommands(object):
     """
     Common commands for the library
     """
+    @pytest.fixture
+    def serf(self):
+        return client.SerfClient()
 
-    def test_has_a_default_host_and_port(self):
-        serf = client.SerfClient()
+    def test_has_a_default_host_and_port(self, serf):
         assert serf.host == 'localhost'
         assert serf.port == 7373
 
-    def test_initialises_a_serf_connection_on_creation(self):
-        serf = client.SerfClient()
+    def test_initialises_a_serf_connection_on_creation(self, serf):
         assert serf.connection is not None
 
-    def test_sending_a_simple_event(self):
-        serf = client.SerfClient()
+    def test_sending_a_simple_event(self, serf):
         assert serf.event('foo', 'bar') == {b'Error': b'', b'Seq': 1}
 
-    def test_sending_a_non_coalescing_event(self):
-        serf = client.SerfClient()
+    def test_sending_a_non_coalescing_event(self, serf):
         assert serf.event('foo', 'bar') == {b'Error': b'', b'Seq': 1}
 
-    def test_event_payload_is_optional(self):
-        serf = client.SerfClient()
+    def test_event_payload_is_optional(self, serf):
         assert serf.event('foo') == {b'Error': b'', b'Seq': 1}
         assert serf.event('bar', coalesce=False) == {b'Error': b'', b'Seq': 2}
 
-    def test_force_leaving_of_a_node(self):
-        serf = client.SerfClient()
+    def test_force_leaving_of_a_node(self, serf):
         assert serf.force_leave('bad-node-name') == {b'Error': b'', b'Seq': 1}
 
-    def test_joining_a_non_existent_node(self):
-        serf = client.SerfClient()
+    def test_joining_a_non_existent_node(self, serf):
         assert serf.join(['127.0.0.1:23000']) == \
             {b'Error': b'dial tcp 127.0.0.1:23000: connection refused',
              b'Seq': 1}
 
-    def test_joining_an_existing_node_fails(self):
-        serf = client.SerfClient()
+    def test_joining_an_existing_node_fails(self, serf):
         assert serf.join(['127.0.0.1:7373']) == \
             {b'Error': b'Reading remote state failed: EOF', b'Seq': 1}
 
-    def test_providing_a_single_value_should_put_it_inside_a_list(self):
-        serf = client.SerfClient()
+    def test_providing_a_single_value_should_put_it_inside_a_list(self, serf):
         assert serf.join('127.0.0.1:7373') == \
             {b'Error': b'Reading remote state failed: EOF', b'Seq': 1}
--- tests/test_connection.py
+++ tests/test_connection.py
@@ -7,9 +7,11 @@ class TestSerfConnection(object):
     """
     Tests for the Serf RPC communication object.
     """
+    @pytest.fixture
+    def rpc(self):
+        return connection.SerfConnection()
 
-    def test_has_a_default_host_port_and_timeout(self):
-        rpc = connection.SerfConnection()
+    def test_has_a_default_host_port_and_timeout(self, rpc):
         assert rpc.host == 'localhost'
         assert rpc.port == 7373
         assert rpc.timeout == 3
@@ -20,8 +22,7 @@ class TestSerfConnection(object):
         assert rpc.port == 455
         assert rpc.timeout == 500
 
-    def test_representation(self):
-        rpc = connection.SerfConnection()
+    def test_representation(self, rpc):
         assert str(rpc) == \
             'SerfConnection<counter=0,host=localhost,port=7373,timeout=3>'
 
@@ -32,18 +33,15 @@ class TestSerfConnection(object):
         assert 'connecting localhost:40000. Connection refused.' \
             in str(exceptionInfo)
 
-    def test_handshake_to_serf_agent(self):
-        rpc = connection.SerfConnection()
+    def test_handshake_to_serf_agent(self, rpc):
         assert rpc.handshake() == {b'Seq': 0, b'Error': b''}
 
-    def test_call_throws_exception_if_socket_none(self):
-        rpc = connection.SerfConnection()
+    def test_call_throws_exception_if_socket_none(self, rpc):
         with pytest.raises(connection.SerfConnectionError) as exceptionInfo:
             rpc.call('members')
         assert 'handshake must be made first' in str(exceptionInfo)
 
-    def test_handshake_and_call_increments_counter(self):
-        rpc = connection.SerfConnection()
+    def test_handshake_and_call_increments_counter(self, rpc):
         assert 'counter=0' in str(rpc)
         rpc.handshake()
         assert 'counter=1' in str(rpc)
